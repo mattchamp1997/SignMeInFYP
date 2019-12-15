af2631d3ff005320840a441a4e8ef44022c8bbc6
mattchamp1997 <c16308053@mydit.ie>
2019-12-15 05:16:32 +0000

    Made Register page Messed with nav drawer

--- app/src/main/java/com/example/signmeinfyp/LoginPage.java
+++ app/src/main/java/com/example/signmeinfyp/LoginPage.java
@@ -2,15 +2,49 @@ package com.example.signmeinfyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity
 {
+    Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.register);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginIntent();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerIntent();
+            }
+        });
     }
+
+    private void registerIntent()
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
+    private void loginIntent()
+    {
+        Intent intent = new Intent(this, MyClasses.class);
+        startActivity(intent);
+    }
+
 }
