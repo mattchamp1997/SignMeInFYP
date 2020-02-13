d4c97abea0243078daf94a444aa78ca0d566a705
mattchamp1997 <c16308053@mydit.ie>
2020-02-13 01:21:23 +0000

    Registration Code update. Yet to comment lines Also renamed some input fields eg: registration password has different name to login password. Stops confusion between the two

--- app/src/main/java/com/example/signmeinfyp/LoginPage.java
+++ app/src/main/java/com/example/signmeinfyp/LoginPage.java
@@ -1,14 +1,16 @@
 package com.example.signmeinfyp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginPage extends AppCompatActivity
 {
+    EditText LoginIdNumber, LoginPassword;
     Button loginButton, registerButton;
 
     @Override
@@ -17,8 +19,18 @@ public class LoginPage extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        LoginIdNumber = findViewById(R.id.LoginIdNumber);
+        LoginPassword = findViewById(R.id.LoginPassword);
+
         loginButton = findViewById(R.id.loginButton);
         registerButton = findViewById(R.id.register);
+
+        LoginIdNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPage.this, Register.class));
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/signmeinfyp/Register.java
+++ app/src/main/java/com/example/signmeinfyp/Register.java
@@ -1,17 +1,20 @@
 package com.example.signmeinfyp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
 
+    EditText fullName, email, RegIdNumber, RegPassword, RegPasswordVerify;
     Button register;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,12 +22,56 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        fullName = findViewById(R.id.fullName);
+        email = findViewById(R.id.email);
+        RegIdNumber = findViewById(R.id.RegIdNumber);
+        RegPassword = findViewById(R.id.RegPassword);
+        RegPasswordVerify = findViewById(R.id.RegPasswordVerify);
         register = findViewById(R.id.registerConfirm);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                returnToLogin();
+                if(fullName.getText().toString().equals("") || email.getText().toString().equals("") || RegPassword.getText().toString().equals(""))
+                {
+                    builder = new AlertDialog.Builder(Register.this);
+                    builder.setTitle("Something went wrong...");
+                    builder.setMessage("Please fill all fields");
+
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+
+                else if (! RegPassword.getText().toString().equals(RegPasswordVerify.getText().toString()))
+                {
+                    builder = new AlertDialog.Builder(Register.this);
+                    builder.setTitle("Something went wrong...");
+                    builder.setMessage("Please ensure passwords match");
+
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            dialog.dismiss();
+                            RegPassword.setText("");
+                            RegPasswordVerify.setText("");
+                        }
+                    });
+
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
+                }
             }
         });
     }
